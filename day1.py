5f68bc239ced5d648eedc4d9f6f695cdcfd3e814
Ismael126895 <Lenojore67@gmail.com>
2024-01-23 13:55:18 +0300

    day 1

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("""
+Ismael
+January 23th 2024
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+I am feeling 😁
+You can follow my progress at replit.com/@IsmaelLennox
+""")
