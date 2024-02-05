030987e0cd9e21b3e0885380bbf4f1e6f178dbf2
Ismael126895 <Lenojore67@gmail.com>
2024-02-05 07:52:06 +0300

    another lambda

--- pyDevEss/lambda.py
+++ pyDevEss/lambda.py
@@ -1,4 +1,4 @@
-#lambda anonymous - function defined withou any name
+#lambda anonymous - function defined without any name
 x=lambda i,y,z: i+y+z
 print(x(1,2,3))
 
@@ -8,3 +8,7 @@ double=new(2)
 triple=new(3)
 print(double(5))
 print(triple(5))
+
+addStuff=lambda val1,val2:(print(f"adding {val1} and {val2}"),val1 +val2) 
+output=addStuff(3,4)
+print(type(output))
