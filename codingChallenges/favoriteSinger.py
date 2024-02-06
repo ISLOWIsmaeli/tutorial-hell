5a28643c356650c2809be1595f86f74739e607a3
Ismael126895 <Lenojore67@gmail.com>
2024-02-06 16:43:20 +0300

    first hackerearth challenge

---
+++ codingChallenges/favoriteSinger.py
@@ -0,0 +1,7 @@
+singers = int(input("Enter the number of singers: "))
+playlist = []
+for i in range(singers):
+    artist = int(input("Input an artist:"))
+    playlist.append(artist)
+    #playlist[i] = input()
+print (playlist)
