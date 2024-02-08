d281f4175bd59f8bf049bb26cf947c0cef779b1c
Ismael126895 <Lenojore67@gmail.com>
2024-02-08 08:31:56 +0300

    finished lists

--- pyDevEss/lists.py
+++ pyDevEss/lists.py
@@ -1,6 +1,7 @@
 fruits = ["fig","banana","lemon","fig"]
 numbers = [1,2,3]
 newFruits = ["Watermelon","Cherry"]
+
 print(fruits)
 fruits[0:2] = ["Orange","lemon"]
 print(fruits)
@@ -8,3 +9,9 @@ fruits.insert(1,"fig")
 print(fruits)
 fruits.extend(newFruits)
 print(fruits)
+fruits.remove("fig")
+fruits.pop(3)
+del fruits[2]
+print(fruits)
+fruits.clear() # clears items in the list
+del fruits # delete whole list
