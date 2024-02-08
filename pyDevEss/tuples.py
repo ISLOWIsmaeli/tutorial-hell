c9cafab2ffbff09d2cb173ad878b18b0fa82e09e
Ismael126895 <Lenojore67@gmail.com>
2024-02-08 08:45:17 +0300

    added tuples file

--- pyDevEss/tuples.py
+++ pyDevEss/tuples.py
@@ -1,3 +1,10 @@
+# a tuple cannot be changed
 fruits = ("fig","banana","lemon")
 numbers = (1,2,3,4)
+integers1=(1,2,3)
+integers2=(4,5,8)
+
+allTuples=integers1+integers2
 print(fruits)
+print(fruits[:0])
+print(allTuples)
