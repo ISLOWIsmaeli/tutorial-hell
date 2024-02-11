733c716c0e4aab7109be62d37575940280475b7b
Ismael126895 <Lenojore67@gmail.com>
2024-02-11 10:11:26 +0300

    accessing set items

--- pyDevEss/sets.py
+++ pyDevEss/sets.py
@@ -1,4 +1,5 @@
-#doesn't allow duplicates, unordered list, randomly organised
+#doesn't allow duplicates, unordered list, randomly organised, you can add but not change item
+# you cannot access items in a set but can print them using for loops
 fruits = {"orange","banana","lemon"}
 print(len(fruits))
 print(fruits)
@@ -10,3 +11,10 @@ print(type(fruits1)) # class dict
 fruits2=set(())
 print(type(fruits2)) # class set
 print(fruitss)
+
+fruits3={"watermelon","cherry","fig"}
+
+for item in fruits3:
+    print(item)
+
+print("fig" in fruits3)
