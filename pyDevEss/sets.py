e8a323305e83bc800a5642c989d3e0d95811d65b
Ismael126895 <Lenojore67@gmail.com>
2024-02-11 10:07:11 +0300

    used set constructor

--- pyDevEss/sets.py
+++ pyDevEss/sets.py
@@ -1,5 +1,12 @@
-#doesn't allow duplicates, unordered list
+#doesn't allow duplicates, unordered list, randomly organised
 fruits = {"orange","banana","lemon"}
 print(len(fruits))
 print(fruits)
 print(type(fruits))
+
+fruitss =set(("orange","banana","lemon")) #use this method to create an empty set
+fruits1={}
+print(type(fruits1)) # class dict
+fruits2=set(())
+print(type(fruits2)) # class set
+print(fruitss)
