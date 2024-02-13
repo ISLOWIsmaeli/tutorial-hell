78913b1df4dd1530f43b4ad6e93936a700befa49
Ismael126895 <Lenojore67@gmail.com>
2024-02-13 16:12:36 +0300

    adding items to dictionary

--- pyDevEss/dict.py
+++ pyDevEss/dict.py
@@ -1,22 +1,28 @@
-#cannot duplicate, ordered
+#cannot duplicate, ordered, changeable
 person = {
     "name":"fred",
     "age":30,
     "country": "Germany",
 }
-x = person["name"]
-y= person.get("name")
-k = person.keys()
-v= person.values()
-i= person.items()
-if "name" in person:
-    print("Yes,It's completed")
+
+person["age"] = 40
+person.update({"age":30})
+person.update({"id":21,"number":50})
+
 print(person)
-print(i)
-print(k)
-print(y)
-print(x)
-print(v)
+# x = person["name"]
+# y= person.get("name")
+# k = person.keys()
+# v= person.values()
+# i= person.items()
+# if "name" in person:
+#     print("Yes,It's completed")
+# print(person)
+# print(i)
+# print(k)
+# print(y)
+# print(x)
+# print(v)
 
 # print(len(person))
 # print(type(person))
