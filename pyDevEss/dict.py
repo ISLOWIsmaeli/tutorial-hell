31af44f79eb870cd83a5e654ad584584536370fa
Ismael126895 <Lenojore67@gmail.com>
2024-02-13 16:05:39 +0300

    extracting values from dictionaries

--- pyDevEss/dict.py
+++ pyDevEss/dict.py
@@ -1,6 +1,22 @@
+#cannot duplicate, ordered
 person = {
     "name":"fred",
     "age":30,
     "country": "Germany",
 }
+x = person["name"]
+y= person.get("name")
+k = person.keys()
+v= person.values()
+i= person.items()
+if "name" in person:
+    print("Yes,It's completed")
 print(person)
+print(i)
+print(k)
+print(y)
+print(x)
+print(v)
+
+# print(len(person))
+# print(type(person))
