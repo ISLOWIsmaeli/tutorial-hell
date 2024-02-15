c42fd3ad61161fcb243ed9303a86928283aa4c19
Ismael126895 <Lenojore67@gmail.com>
2024-02-15 19:04:27 +0300

    else can be added in python while loops

---
+++ pyDevEss/loops.py
@@ -0,0 +1,10 @@
+# continue and break keywords.
+# you cannot use else statement when you use break in python
+x=0
+while x <7:
+    x = x + 1
+    if x == 3 :
+        continue
+    print(x)
+else:
+    print("x is no longer less than 7")
