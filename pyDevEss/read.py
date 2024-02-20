0de29bb08a7989be5482d24a95c4d671ded01302
Ismael126895 <Lenojore67@gmail.com>
2024-02-20 15:44:57 +0300

    read first file

---
+++ pyDevEss/read.py
@@ -0,0 +1,5 @@
+import os
+os.chdir("c:\\Users\\Lennox\\Documents\\myProjects\\tutorials\\tutorial-hell\\pyDevEss")
+# file = open(r"c:\Users\Lennox\Documents\myProjects\tutorials\tutorial-hell\pyDevEss\fileManipulations\main.txt","r")
+file = open("main.txt")
+print(file.read())
