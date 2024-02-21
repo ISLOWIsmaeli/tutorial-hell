5b2bd1140117c459396f3d6252ca1cb06bd8fc40
Ismael126895 <Lenojore67@gmail.com>
2024-02-21 13:05:04 +0300

    final read file

--- pyDevEss/read.py
+++ pyDevEss/read.py
@@ -1,5 +1,4 @@
 import os
 os.chdir("c:\\Users\\Lennox\\Documents\\myProjects\\tutorials\\tutorial-hell\\pyDevEss")
-# file = open(r"c:\Users\Lennox\Documents\myProjects\tutorials\tutorial-hell\pyDevEss\fileManipulations\main.txt","r")
-file = open("main.txt")
+file = open("main.txt","r")
 print(file.read())
