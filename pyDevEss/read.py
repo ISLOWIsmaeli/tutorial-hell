72f1a1a813a401f34ba12efb0385afdb0c962bec
Ismael126895 <Lenojore67@gmail.com>
2024-02-21 13:09:49 +0300

    finished writiting to files

--- pyDevEss/read.py
+++ pyDevEss/read.py
@@ -1,4 +1,10 @@
 import os
 os.chdir("c:\\Users\\Lennox\\Documents\\myProjects\\tutorials\\tutorial-hell\\pyDevEss")
 file = open("main.txt","r")
-print(file.read())
+print(file.read(20))
+file.close()
+print(file.readline())
+print(file.readline())
+
+for x in file:
+    print(x)
