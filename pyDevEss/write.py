f62e9207f2b0edcd284b2007a62bdfb40c0edca8
Ismael126895 <Lenojore67@gmail.com>
2024-02-21 13:18:58 +0300

    removing files

--- pyDevEss/write.py
+++ pyDevEss/write.py
@@ -1,8 +1,9 @@
 import os
 os.chdir("c:\\Users\\Lennox\\Documents\\myProjects\\tutorials\\tutorial-hell\\pyDevEss")
 # file = open("write.txt","a")#add content
-file = open("write.txt","w") # overwrite content
-file.write("Writing new  to this text file")
-file.close()
+# file = open("write.txt","w") # overwrite content
+# file.write("Writing new  to this text file")
+# file.close()
 
-file = open("demo.txt","x")
+# file = open("demo.txt","x")
+os.remove("demo.txt")
