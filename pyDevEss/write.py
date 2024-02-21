518717d04b620285ce122977e7c6e25d4a464e6f
Ismael126895 <Lenojore67@gmail.com>
2024-02-21 13:16:46 +0300

    created a new text file

--- pyDevEss/write.py
+++ pyDevEss/write.py
@@ -1,6 +1,8 @@
 import os
 os.chdir("c:\\Users\\Lennox\\Documents\\myProjects\\tutorials\\tutorial-hell\\pyDevEss")
 # file = open("write.txt","a")#add content
-file = open("write.txt","a") # overwrite content
+file = open("write.txt","w") # overwrite content
 file.write("Writing new  to this text file")
 file.close()
+
+file = open("demo.txt","x")
