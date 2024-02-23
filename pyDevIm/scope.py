e600b9b995d7211dd83052bf8755f564e8ac11d6
Ismael126895 <Lenojore67@gmail.com>
2024-02-23 15:28:37 +0300

    global variables

--- pyDevIm/scope.py
+++ pyDevIm/scope.py
@@ -1,6 +1,13 @@
-def newOut():
-    x=200
-    def newIn():
-        print(x)
-    newIn()
-newOut()
+# def newOut():
+#     x=200
+#     def newIn():
+#         print(x)
+#     newIn()
+# newOut()
+x =100
+
+def newfunc():
+    print(x)
+
+newfunc()
+print(x)
