9b5c9609065f6e3270a043f6bea7b68f9b6e6aba
Ismael126895 <Lenojore67@gmail.com>
2024-02-23 15:41:32 +0300

    adding multiple parameters

--- pyDevIm/format.py
+++ pyDevIm/format.py
@@ -1,3 +1,8 @@
-number=25
-newmsg="The number is {:.2f} for you!"
-print(newmsg.format(number))
+# number=25
+# newmsg="The number is {:.2f} for you!"
+# print(newmsg.format(number))
+quantity =5
+itemNumber = 50
+price =24
+firstOrder ="We want {} pieces of item number {} for {:.2f} dollars."
+print(firstOrder.format(quantity,itemNumber, price))
