f3f617f621880e267b095b953b1bb43ae46dcb0a
Ismael126895 <Lenojore67@gmail.com>
2024-02-25 22:12:09 +0300

    using named indexes

--- pyDevIm/format.py
+++ pyDevIm/format.py
@@ -1,14 +1,16 @@
 # number=25
 # newmsg="The number is {:.2f} for you!"
 # print(newmsg.format(number))
-quantity =5
-itemNumber = 50
-price =24
-age = 45
-name = "Jack"
-newTxt = "The name is {1}. {1} is {0} years old"
-firstOrder = "We want {} pieces of item number {} for {:.2f} dollars."
-secondOrder = "We want {0} pieces of item number {1} for {2:.2f} dollars."
-print(firstOrder.format(quantity,itemNumber, price))
-print(secondOrder.format(quantity,itemNumber, price))
-print(newTxt.format(age, name))
+# quantity =5
+# itemNumber = 50
+# price =24
+# age = 45
+# name = "Jack"
+# newTxt = "The name is {1}. {1} is {0} years old"
+# firstOrder = "We want {} pieces of item number {} for {:.2f} dollars."
+# secondOrder = "We want {0} pieces of item number {1} for {2:.2f} dollars."
+# print(firstOrder.format(quantity,itemNumber, price))
+# print(secondOrder.format(quantity,itemNumber, price))
+# print(newTxt.format(age, name))
+newOrder = "I have a {car}, it is a {model}"
+print(newOrder.format(car="Volvo",model ="S60"))
