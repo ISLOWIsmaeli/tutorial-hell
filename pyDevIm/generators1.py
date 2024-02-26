daef00a9ef4bce834d0a32229d271c41ee276b8e
Ismael126895 <Lenojore67@gmail.com>
2024-02-26 10:27:06 +0300

    a generator is more efficient than an iterator

--- pyDevIm/generators1.py
+++ pyDevIm/generators1.py
@@ -10,11 +10,13 @@
 # print(next(seql))
 # print(next(seql))
 def squareSequence(x):
-    for i in range(x+1):
+    for i in range(x):
         yield i*i
 newGenerat=squareSequence(7)
-while True:
-    try:
-        print("Receive on the next()",next(newGenerat))
-    except StopIteration:
-        break  
+# while True:
+#     try:
+#         print("Receive on the next()",next(newGenerat))
+#     except StopIteration:
+#         break  
+for sqr in newGenerat:
+    print(sqr)
