de6c3fc59113d341bec24a2765f4544fb0af9563
Ismael126895 <Lenojore67@gmail.com>
2024-02-26 10:25:17 +0300

    using try catch block to control yield

--- pyDevIm/generators1.py
+++ pyDevIm/generators1.py
@@ -1,11 +1,20 @@
-def newUpTo(x):
-    for i in range(x):
-        yield i
-seql= newUpTo(7)
-print(next(seql))
-print(next(seql))
-print(next(seql))
-print(next(seql))
-print(next(seql))
-print(next(seql))
-print(next(seql))
+# def newUpTo(x):
+#     for i in range(x):
+#         yield i
+# seql= newUpTo(7)
+# print(next(seql))
+# print(next(seql))
+# print(next(seql))
+# print(next(seql))
+# print(next(seql))
+# print(next(seql))
+# print(next(seql))
+def squareSequence(x):
+    for i in range(x+1):
+        yield i*i
+newGenerat=squareSequence(7)
+while True:
+    try:
+        print("Receive on the next()",next(newGenerat))
+    except StopIteration:
+        break  
