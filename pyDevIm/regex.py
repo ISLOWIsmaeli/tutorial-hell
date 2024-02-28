8304150cba39d38e8611b973d6a5c55f4e67b930
Ismael126895 <Lenojore67@gmail.com>
2024-02-28 22:01:56 +0300

    regex file

--- pyDevIm/regex.py
+++ pyDevIm/regex.py
@@ -1,10 +1,11 @@
-# A string of characters that form a search pattern
-import re
-# Search the string to see if it starts with "You" and ends with "Python"
-newTxt = "You can start learning Python"
-x = re.search("^You.*Python$",newTxt)
+# # A string of characters that form a search pattern
+# import re
+# # Search the string to see if it starts with "You" and ends with "Python"
+# newTxt = "You can start learning Python"
+# x = re.search("^You.*Python$",newTxt)
 
-if x:
-    print("Yes, we have a match!")
-else:
-    print("No match!")
+# if x:
+#     print("Yes, we have a match!")
+# else:
+#     print("No match!")
+# metacharacters,findall,search,sub,split
