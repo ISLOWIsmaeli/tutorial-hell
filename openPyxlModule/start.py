107738771082edaba3a21a19aba4bc9af63030f9
Ismael126895 <Lenojore67@gmail.com>
2024-02-28 22:40:08 +0300

    ...

--- openPyxlModule/start.py
+++ openPyxlModule/start.py
@@ -1,5 +1,6 @@
 from openpyxl import workbook, load_workbook
 
 wb = load_workbook('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
-
+ws = wb.active
+print(ws['A1'].value)
 
