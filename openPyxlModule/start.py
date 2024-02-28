ff3d2d91c7e02bb426e98213177fcd5b2f74ab12
Ismael126895 <Lenojore67@gmail.com>
2024-02-28 22:33:17 +0300

    openpyxl modules

--- openPyxlModule/start.py
+++ openPyxlModule/start.py
@@ -1,3 +1,5 @@
 from openpyxl import workbook, load_workbook
 
-wb = load_workbook('')
+wb = load_workbook('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
+
+
