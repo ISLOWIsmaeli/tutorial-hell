e64fa7766254462f39ffed00754bf06ec9e9dfff
Ismael126895 <Lenojore67@gmail.com>
2024-03-04 21:22:35 +0300

    openpyxl updates

--- openPyxlModule/start.py
+++ openPyxlModule/start.py
@@ -1,15 +1,16 @@
 from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
 
 # wb = load_workbook('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
 # ws = wb['Grades']
 # wb.create_sheet("Test")
 # print(wb.sheetnames)
+wb = load_workbook('ismael.xlsx')
 wb=Workbook()
 ws=wb.active
-ws.title = "Data"
-
-ws.append(['Lennox','Is','Great','!'])
-ws.append(['Lennox','Is','Great','!'])
-ws.append(['Lennox','Is','Great','!'])
-ws.append(['End',])
+for row in range(1,11):
+    for col in range (1,5):
+        # char = chr(65 + col) the manual way, use range 1 to 5
+        char = get_column_letter(col)
+        print(ws[char + str(row)].value)
 wb.save('ismael.xlsx')
