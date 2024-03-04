aee7274192bafb0cfdab17098f4f93b02acbc40a
Ismael126895 <Lenojore67@gmail.com>
2024-03-04 21:10:29 +0300

    created a new workbook

--- openPyxlModule/start.py
+++ openPyxlModule/start.py
@@ -1,8 +1,12 @@
-from openpyxl import workbook, load_workbook
+from openpyxl import Workbook, load_workbook
 
-wb = load_workbook('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
-ws = wb.active
-print(ws['A1'].value)
-ws['A2'].value = "Test"
-wb.save('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
+# wb = load_workbook('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
+# ws = wb['Grades']
+# wb.create_sheet("Test")
+# print(wb.sheetnames)
+wb=Workbook()
+ws=wb.active
+ws.title = "Data"
 
+ws.append(['Lennox','Is','Great','!'])
+wb.save('ismael.xlsx')
