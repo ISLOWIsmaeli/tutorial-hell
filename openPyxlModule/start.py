133771da40fa0c10a41fb3dd3b5e9d194c80c837
Ismael126895 <Lenojore67@gmail.com>
2024-03-04 20:53:19 +0300

    assign value to a cell

--- openPyxlModule/start.py
+++ openPyxlModule/start.py
@@ -3,4 +3,6 @@ from openpyxl import workbook, load_workbook
 wb = load_workbook('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
 ws = wb.active
 print(ws['A1'].value)
+ws['A2'].value = "Test"
+wb.save('C:\\Users\\Lennox\\documents\\myProjects\\tutorials\\tutorial-hell\\openPyxlModule\\grades.xlsx')
 
