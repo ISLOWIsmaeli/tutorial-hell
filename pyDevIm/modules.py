de2047446e856da21fc5191f65a1d38b48a22b15
Ismael126895 <Lenojore67@gmail.com>
2024-03-06 10:55:11 +0300

    done with os module

---
+++ pyDevIm/modules.py
@@ -0,0 +1,4 @@
+#python file containing definition of class, function, code to be used in another code
+#built in modules are written in C and integrated with the python interpreter
+#each module contains inbuilt resources for certain functionalities e.g os management.
+#help("modules") see available built in modules
--- pyDevIm/osModule.py
+++ pyDevIm/osModule.py
@@ -2,3 +2,6 @@
 import os
 os.mkdir("")#make a directory
 os.chdir("")#change to a particular directory
+os.getcwd()#checks if directory exists
+os.rmdir("")#removes directory
+os.listdir("path")#gives a list of directories in the current path
