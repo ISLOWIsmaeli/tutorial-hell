714b88115672959ab004f1073f94eefad6b95fe7
Ismael126895 <Lenojore67@gmail.com>
2024-03-08 09:19:36 +0300

    using oredered dictionaries

--- pyDevIm/collModule.py
+++ pyDevIm/collModule.py
@@ -1,4 +1,12 @@
 import collections
-player=collections.namedtuple("player",["name","age","team"])
+player=collections.namedtuple("player",["name","age","team"])#it required a field name
 p1=player("Ian",44,"Kenya")
 print(p1.name,p1.age,p1.team,sep=",,")
+#ordered dict remembers the order of the keys by the time they were first inserted
+d1=collections.OrderedDict()
+d1["First: "]=35
+d1["Second: "]=40
+d1["Third: "]=45
+d1["Fourth: "]=50
+for k,v in d1.items():
+    print(k,v)
