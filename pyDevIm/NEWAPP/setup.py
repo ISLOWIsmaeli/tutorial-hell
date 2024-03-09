45aa207b1b0e5f5fce304d9373747e04af6d646d
Ismael126895 <Lenojore67@gmail.com>
2024-03-09 20:11:05 +0300

    made package global

---
+++ pyDevIm/NEWAPP/setup.py
@@ -0,0 +1,13 @@
+#installing the new package globally
+from setuptools import setup
+setup(
+    name='newpackage',
+    version='0.1',
+    description='Testing installation of newpackage',
+    url='#',
+    author='Ismael LSO',
+    author_email='',
+    license='',
+    packages=['newpackage'],
+    zip_safe=False
+)
