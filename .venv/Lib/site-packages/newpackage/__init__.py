2b4f1ba436793305dbd885160fabb6bb5fef2f22
Ismael126895 <Lenojore67@gmail.com>
2024-03-09 21:16:04 +0300

    untracked changes

---
+++ .venv/Lib/site-packages/newpackage/__init__.py
@@ -0,0 +1,4 @@
+# makes a folder to be recognised as a package
+# can also be used to choose what is supposed to be imported
+from .newfunctions import average, power
+from .greetings import greeting
