bf3bdfe8f8d8a7aff3428ed7727ce1bea5380989
Ismael126895 <Lenojore67@gmail.com>
2024-03-11 17:57:13 +0300

    completed madlibs challenge

--- pyDevIm/madlibs.py
+++ pyDevIm/madlibs.py
@@ -7,8 +7,13 @@ adjective=input("Enter an adjective: ")
 noun2=input("Enter noun2: ")
 adverb=input("Enter an adverb: ")
 verb=input("Enter a verb: ")
+if (noun1[0].lower()) in (['a','e','i','o','u']) :
+    noun1=noun1.title()
+    noun2=noun2.title()
+    print("""Welcome to the whimsical world of Madlibs! Prepare to laugh hysterically as you fill in the blanks with nouns, adjectives, and verbs to concoct a delightfully absurd story. First, give me a noun: {}. Now, an adjective:{}. Followed by another noun: {}. How about an adverb this time: {}. And finally, a verb ending in 'ing': {}. Now let's unveil the uproarious narrative we've crafted together!""".format(noun1,adjective,noun2,adverb,verb))
+else:
+    print("Game over!!!")
 
-noun1.title
-noun2.title
 
-print("""Welcome to the whimsical world of Madlibs! Prepare to laugh hysterically as you fill in the blanks with nouns, adjectives, and verbs to concoct a delightfully absurd story. First, give me a noun: {}. Now, an adjective:{}. Followed by another noun: {}. How about an adverb this time: {}. And finally, a verb ending in 'ing': {}. Now let's unveil the uproarious narrative we've crafted together!""".format(noun1,adjective,noun2,adverb,verb))
+
+
