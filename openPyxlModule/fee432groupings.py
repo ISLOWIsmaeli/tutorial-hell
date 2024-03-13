4b71fc30c69d1bdf1e82728e8b55e9aff60f9683
Ismael126895 <Lenojore67@gmail.com>
2024-03-13 14:10:26 +0300

    updated sorting algo

--- openPyxlModule/fee432groupings.py
+++ openPyxlModule/fee432groupings.py
@@ -7,6 +7,8 @@ class_list_sheet=workbook[mysheets[0]]
 projects_without_ismael_sheet=workbook[mysheets[1]]
 projects_with_ismael_sheet=workbook[mysheets[2]]
 
+classmates_with_groups=[]
+
 for class_list in class_list_sheet.iter_rows(min_row=3,
                                              max_row=128,
                                              min_col=2,
@@ -17,26 +19,13 @@ for class_list in class_list_sheet.iter_rows(min_row=3,
                                                                   max_row=120,
                                                                   min_col=3,
                                                                   max_col=3,
-                                                                  values_only=True):
+                                                                values_only=True):
         classmate2=without_ismael[0]
         if classmate1==classmate2:
-            continue
+            print(classmate1,"has a group")
+            classmates_with_groups.append(classmate1)
+            break
         else:
-            print(classmate1)
+            pass
     
-
-# sheet1 = workbook.active
-# print(mysheets)
-
-# print(sheet1["C2:C12"].value)
-# for reg_no in sheet1.iter_rows(min_row=2,
-#                             max_row=120,
-#                             min_col=3,
-#                             max_col=3,
-#                             values_only=True):
-#     string=reg_no[0]
-#     if string == "F17/2010/2020":
-#         print("Armstrong found!!")
-#     else:
-#         print("Please think again bro")
-#     # print(string,sep="\n")
+print(classmates_with_groups)
