8ca0cc664d8d9d8e395aeca3ec9a846a34858dba
Ismael126895 <Lenojore67@gmail.com>
2024-03-18 22:35:23 +0300

    finished site entry prompt

--- openPyxlModule/siteSorting.py
+++ openPyxlModule/siteSorting.py
@@ -4,3 +4,10 @@ import random
 filename="testData.xlsx"
 workbook=load_workbook(filename)
 
+no_of_sites=int(input("Please enter the number of sites: "))
+site_names =[]
+
+for i in range(no_of_sites):
+    site_entry=input("Site {}".format(i+1))
+
+
