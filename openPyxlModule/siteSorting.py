d383eadee27edce2b69d8495ba19e0100663e927
Ismael126895 <Lenojore67@gmail.com>
2024-03-18 22:11:14 +0300

    initiated the sorting script

--- openPyxlModule/siteSorting.py
+++ openPyxlModule/siteSorting.py
@@ -1,6 +1,9 @@
 from openpyxl import load_workbook
 import random
 
+filename="testData.xlsx"
+workbook=load_workbook(filename)
+
 sites=int(input("Enter the number of sites"))
 
 for i in range(sites):
