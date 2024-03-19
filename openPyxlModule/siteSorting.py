053279b52ce60eb3248e1fa4b73baf5d7cacff1b
Ismael126895 <Lenojore67@gmail.com>
2024-03-19 10:51:21 +0300

    attempted a new random extraction method

--- openPyxlModule/siteSorting.py
+++ openPyxlModule/siteSorting.py
@@ -4,7 +4,7 @@ import random
 filename="testData.xlsx"
 workbook=load_workbook(filename)
 
-sheet1=workbook["sheet1"]
+sheet1=workbook["Sheet1"]
 no_of_sites=int(input("Please enter the number of sites: "))
 site_names =[]
 missionary_list=[]
@@ -21,4 +21,17 @@ for missionary in sheet1.iter_rows(min_row=2,
                                    max_col=3,
                                    values_only=True):
     #to iterate through all the missionaries and store them in a list
-    print(missionary)
+    missionary_list.append(missionary)
+    # print(missionary)
+print(random.choice(missionary_list))
+
+for missionary in missionary_list:
+    pick=random.choice(missionary_list)
+    index= missionary_list.index(pick)
+    for confirm in missionary_list:
+        if pick == confirm:
+            del missionary_list[index]
+            continue
+        elif ValueError:
+            continue
+print(missionary_list)
