4abefdd609285be57fde594a7e16b75ad32d3b2d
Ismael126895 <Lenojore67@gmail.com>
2024-04-13 23:29:04 +0300

    stepy

--- numAss/elimination.py
+++ numAss/elimination.py
@@ -1,3 +1,10 @@
 #come up with a 3 by 4 matrix
 #eliminate using gausss jordan
 #produce the answer
+trial=[[2,1,2,8],[2,-3,3,-4],[4,2,-1,1]]
+matrix=[[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+print("Please enter a 3 variable simultaneous equation matrix")
+for i in range(0,3,1):
+    for j in range(0,4,1):
+        print(trial[i][j])
+
